fcdd04a05f113d75ffaf5bd23b95f628f9734eab
Evan Machnic <emachnic@engineyard.com>
2013-09-10 12:17:26 -0400

    rails_apps_composer: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 CodetvNginx::Application.routes.draw do
-  get "home/index"
+  root :to => "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
