1073659fe19ebd003bfdce51d361c9721b111635
Evan Machnic <emachnic@engineyard.com>
2013-09-10 12:08:54 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CodetvNginx::Application.config.secret_key_base = '54abfea9d127756c5d2b13c2eb534422a05727f3727b73845b351d499bf32bc88cfd0fb46800aeb65bb4c19def5968e9ea6dc49b813392b611bc62e4930d86ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CodetvNginx::Application.config.session_store :cookie_store, key: '_codetv_nginx_session'
