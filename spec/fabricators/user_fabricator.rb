b49b203ebd0e7d37a64a3a6f548606427425f0ba
Evan Machnic <emachnic@engineyard.com>
2013-09-10 12:18:34 -0400

    rails_apps_composer: set up database

---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,8 @@
+Fabricator(:user) do
+  name     'Test User'
+  email    'example@example.com'
+  password 'changeme'
+  password_confirmation 'changeme'
+  # required if the Devise Confirmable module is used
+  # confirmed_at Time.now
+end
