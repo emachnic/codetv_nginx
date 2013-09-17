61e96e7c4249d062b1d02f7ff2459964f240702a
Evan Machnic <emachnic@engineyard.com>
2013-09-17 10:34:58 -0400

    Add additonal packages

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -9,6 +9,7 @@
 
 if platform? 'ubuntu', 'debian'
 	%w{nodejs ruby1.9.1-dev zlib1g-dev git-core sqlite3 libsqlite3-dev}.each do |pkg_name|
+	%w{nodejs ruby1.9.1-dev zlib1g-dev git-core sqlite3 libsqlite3-dev libxml2-dev libxslt1-dev libffi-dev}.each do |pkg_name|
 		package pkg_name do
 			action :install
 		end
